55611fc154e686eae186b4d1d821caeb832fbb7b
26erika <develop.erika@gmail.com>
2019-12-05 17:25:20 -0600

    results view

--- src/Components/Recruiter/Admin-Pages/Area.js
+++ src/Components/Recruiter/Admin-Pages/Area.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import Logo from "../../../Images/astra-logo.png";
 
 class Area extends Component {
   render() {
  
     return (
         
-        <div>
-            
+        <div className= "results">
+            <img className="logo-astra" src={Logo} alt="logo" />
+            <div className="container-login-recruiter">
+                <h2 className="title-area">Candidates who passed the test.</h2>
+                <div></div>
+            </div>
         </div>
     )
   }
