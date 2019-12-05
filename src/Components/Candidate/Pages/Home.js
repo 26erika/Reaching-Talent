64d944b803040d8f33dc0bf94e6843b145c99535
PaulinaFranco <dadiante@gmail.com>
2019-12-05 17:24:41 -0600

    Add terms and conditions checkbox

--- src/Components/Candidate/Pages/Home.js
+++ src/Components/Candidate/Pages/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Logo from "../../../Images/astra-logo.png";
 
 class LoginForm extends React.Component {
   constructor(props) {
@@ -20,17 +21,29 @@ class LoginForm extends React.Component {
 
 render() {
   return (
-    <div>
+    <div className="loginForm">
+      <img className="logo-astra" src={Logo} alt="logo" />
       <h1>Reaching Talent</h1>
 
-      <form onSubmit={this.handleSubmit}>
-          <label>
-            Email
+      <p>Guessing if you can be part of AstraZeneca's family?</p>
+      <p>Ẃelcome to our app.....</p>
 
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+    <div>
+        <form onSubmit={this.handleSubmit}>
+            <label>
+              Email
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          
+          <div class="field">
+            <div class="ui checkbox">
+              <input type="checkbox" class="hidden" readonly="" tabindex="0" />
+              <label>I agree to the Terms and Conditions</label>
+            </div>
+          </div>
+          <button type="submit" class="button">Submit</button>
+        </form>
+      </div>
 
     </div>
     )
