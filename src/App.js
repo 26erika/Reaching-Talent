7b8bb48d6011fa413764158d2e522bf7e98b5146
26erika <develop.erika@gmail.com>
2019-12-05 15:41:11 -0600

    add logo and structure of recruiter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'semantic-ui-react'
-
-const ButtonExampleButton = () => <Button>Click Here</Button>
-
-
+import RecruiterLogin from './Components/Recruiter/Admin-Pages/Recruiter-Home';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <button class="ui button">Click Here</button>
-    </div>
+    <RecruiterLogin/>
+    
   );
 }
 
---
+++ src/Components/Recruiter/Admin-Pages/Area.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Area extends Component {
+  render() {
+ 
+    return (
+        
+        <div>
+            
+        </div>
+    )
+  }
+}
+export default Area;
