768122e426b5d0810f11382e780e9f568e30630e
26erika <develop.erika@gmail.com>
2019-12-06 08:06:29 -0600

    Updates

---
+++ precache-manifest.04223bfea7af24662fb16d6c49dd6f3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce5f87515cd63ad678b46ed7f3654ff",
+    "url": "/Reaching-Talent/index.html"
+  },
+  {
+    "revision": "80a96576f8ebfbcabfbd",
+    "url": "/Reaching-Talent/static/css/main.7482a0ec.chunk.css"
+  },
+  {
+    "revision": "71f8d8b7e2459d6d1847",
+    "url": "/Reaching-Talent/static/js/2.2028300b.chunk.js"
+  },
+  {
+    "revision": "0d93e77bfe30b662623a4c7229c08be0",
+    "url": "/Reaching-Talent/static/js/2.2028300b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80a96576f8ebfbcabfbd",
+    "url": "/Reaching-Talent/static/js/main.b35e37a1.chunk.js"
+  },
+  {
+    "revision": "8f3e76087bd9ffcebb24",
+    "url": "/Reaching-Talent/static/js/runtime-main.5c37397f.js"
+  },
+  {
+    "revision": "dcddc6b58cf2a77134e6056bbfd9047c",
+    "url": "/Reaching-Talent/static/media/astra-logo.dcddc6b5.png"
+  }
+]);
