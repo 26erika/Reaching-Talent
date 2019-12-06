267886d96e838c52e1a8b1f44f43bedd6c6b8c9f
PaulinaFranco <dadiante@gmail.com>
2019-12-05 20:02:33 -0600

    Start test view component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import RecruiterLogin from './Components/Recruiter/Admin-Pages/Recruiter-Home';
 
 
 import LoginForm from './Components/Candidate/Pages/Home.js';
-
+import TalentTest from './Components/Candidate/Pages/Talent-Test.js';
 function App() {
   return (
 
   <div>
     < LoginForm />
+    < TalentTest />
 
     <RecruiterLogin/>
   </div>
