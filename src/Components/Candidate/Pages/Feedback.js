249d3d8d6705fdd8515609fc4c2550f6e5e007cd
karo0wk <karo0wk@gmail.com>
2019-12-06 14:29:06 -0600

    Add Page Feedback

---
+++ src/Components/Candidate/Pages/Feedback.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import Axios from 'axios';
+import Logo from '../../../Images/astra-logo.png';
+import Check from '../../../Images/checked.png';
+import Data from '../Elements/dataFalse.js';
+import QuestionsContainer from '../Elements/QuestionsContainer.js';
+import baseURL from '../../../Constant/Env';
+
+
+class Feedback extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          results: null
+        };
+      }
+    //   componentDidMount() {
+    //     Axios.get(baseURL + `/recruiter`)
+    //       .then(res => {
+    //         const results = JSON.parse(res.data);
+    //         this.setState({ results });
+    //       })
+    //       .catch(e => {
+    //         console.log(e);
+    //       });
+    //   }
+    //   handleClick() {
+    //     console.log(this.state.results);
+    //   }
+    
+      render() {
+        
+          return (
+             
+            <div >
+                 <div className="headeR"> 
+        </div>
+                
+                
+              <div class="divFeed1">
+                        <div className="imgAZ">
+                          <img className="logo-astra"  src={Logo} alt="logo" />
+                       </div>
+                  
+                  <h2 className="textNormal">You have completed all tests! </h2>
+                  <div>
+                  <h3 className="textNormal" >You have passed the tests. 
+                  <img   src={Check} alt="logo" /></h3>
+                   <p className="textNormal" > Congratulations!
+                   </p>
+                   <p className="textNormal" >
+                   We will contact you as soon as possible for a personal interview</p>
+              <textarea placeholder="Write your feedback" className="texArea1"></textarea>
+              <button className="butt">Send and finalize</button>
+              </div>
+              </div>
+            </div>
+          );
+        }
+      }
+    
+
+  export default Feedback;
